4c37dd5547c68556b6832f822e7c819aec177fe7
jongwook-song <jssongjk637@gmail.com>
2020-12-02 05:00:59 +0900

    react move

--- src/test/java/com/jk/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jk/book/springboot/web/HelloControllerTest.java
@@ -13,35 +13,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.is;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
-
-    @Test
-    public void hello_리턴된다() throws Exception{
-        String hello = "hello";
-
-        mvc.perform( get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
-    }
-
-    @Test
-    public void helloDto가_리턴된다() throws Exception{
-        String name = "hello";
-        int amount = 1000;
-
-        mvc.perform( get("/hello/dto")
-        .param("name", name)
-        .param("amount", String.valueOf( amount)))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.name", is(name)))
-        .andExpect(jsonPath("$.amount", is(amount)));
-
-
-    }
+//    @Autowired
+//    private MockMvc mvc;
+//
+//    @Test
+//    public void hello_리턴된다() throws Exception{
+//        String hello = "hello";
+//
+//        mvc.perform( get("/hello"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(hello));
+//    }
+//
+//    @Test
+//    public void helloDto가_리턴된다() throws Exception{
+//        String name = "hello";
+//        int amount = 1000;
+//
+//        mvc.perform( get("/hello/dto")
+//        .param("name", name)
+//        .param("amount", String.valueOf( amount)))
+//        .andExpect(status().isOk())
+//        .andExpect(jsonPath("$.name", is(name)))
+//        .andExpect(jsonPath("$.amount", is(amount)));
+//
+//
+//    }
 
 }
